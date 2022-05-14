3b49030b5c423ba99144962523b82ccca5781572
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 16:28:00 -0600

    data file cahnges to lorem ipsum

--- setup/src/data.js
+++ setup/src/data.js
@@ -6,7 +6,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in risus vel mi dictum tincidunt quis a tortor. Integer elementum purus odio, a lobortis purus malesuada at. Proin sodales ipsum.",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae diam varius, sodales est vel, pellentesque odio. Nunc nec ante sollicitudin nunc accumsan consequat eget sed ligula. Sed interdum ullamcorper.',
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac sem eget arcu mattis blandit vitae vel magna. Proin sagittis bibendum elit, non eleifend dui pharetra eu. Aliquam molestie aliquet.',
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec tristique nunc. Curabitur ac accumsan lorem. Aliquam nec auctor est. Aenean non orci lacinia, congue est vitae, mollis enim. Phasellus.',
   },
 ];
 
