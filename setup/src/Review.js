d86ec46fada2d0abcb85e68e5b7339fceaac170b
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 16:24:06 -0600

    added in next person and previous person buttons, as well as a random person button.  each function uses Math to ensure we dont navigate to an invalid index and the random button just uses Math.random to find an index

--- setup/src/Review.js
+++ setup/src/Review.js
@@ -6,6 +6,38 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1){
+      return 0
+    }
+    if (number < 0){
+      return people.length - 1
+    }
+    return number
+  }
+
+const nextPerson = () => {
+  setIndex((index)=>{
+    let newIndex = index + 1;
+    return checkNumber(newIndex);
+  });
+};
+
+const prevPerson = () => {
+  setIndex((index)=>{
+    let newIndex = index - 1;
+    return checkNumber(newIndex);
+  });
+};
+
+const randomPerson = () => {
+let randomNumber = Math.floor(Math.random() * people.length)
+if (randomNumber == index){
+  randomNumber = index + 1
+}
+setIndex(checkNumber(randomNumber))
+}
+
   return (
     <article className="review">
       <div className="img-container">
@@ -18,10 +50,16 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        
+        <button className="prev-btn" onClick={() => prevPerson()}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={() => nextPerson()}>
+          <FaChevronRight />
+        </button>
       </div>
-      
+      <button className="random-btn" onClick={() => randomPerson()}>Surprise Me</button>
     </article>
+  );
 };
 
 export default Review;
