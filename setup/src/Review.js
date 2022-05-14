0d185c291bf610a900045a98d0bd6cc0134fa260
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 16:18:35 -0600

    initial commit with react app and files included

---
+++ setup/src/Review.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  
+};
+
+export default Review;
