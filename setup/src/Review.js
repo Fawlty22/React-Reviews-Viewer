3f8e88296a8f3a30a9ae89ec6044b7cc9d730033
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 16:22:52 -0600

    set up basic article element with associated data for the indexed person we want to see.  State tracks index, next i will add buttons to see next and previous person, as well as functionality for said buttons

--- setup/src/Review.js
+++ setup/src/Review.js
@@ -3,7 +3,25 @@ import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        
+      </div>
+      
+    </article>
 };
 
 export default Review;
